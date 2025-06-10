c3c3907d942e46a8217e6638ce5496cd6cb6d907
don-dempsey <97637401+don-dempsey@users.noreply.github.com>
2025-06-10 17:32:26 -0500

    Final changes before pull request.

---
+++ app_specs/TreeSort.md
@@ -0,0 +1,39 @@
+# Application specification: TreeSort
+
+This is the application specification for the service with identifier TreeSort.
+
+The backend script implementing the application is [App-TreeSort.pl](../service-scripts/App-TreeSort.pl).
+
+The raw JSON file for this specification is [TreeSort.json](TreeSort.json).
+
+This service performs the following task: TreeSort infers both recent and ancestral reassortment events along the branches of a phylogenetic tree of a fixed genomic segment.
+
+It takes the following parameters:
+
+| id | label | type | required | default value |
+| -- | ----- | ---- | :------: | ------------  |
+| clades_path | Clades path | string | ??? | |
+| deviation | Deviation | float? | | 2 |
+| equal_rates | Equal rates | boolean | | |
+| input_fasta_data | Input FASTA data | string | | |
+| input_fasta_existing_dataset | Existing dataset | string | | |
+| input_fasta_file_id | Input FASTA file ID | wsid | | |
+| input_fasta_group_id | Input genome group | wsid | | |
+| input_source | Input source | fasta_data, fasta_existing_dataset, fasta_file_id, fasta_group_id | :heavy_check_mark: | fasta_file_id |
+| is_time_scaled | Is time scaled? | boolean | | false |
+| match_regex | A regular expression for matching | string (regex) |  |  |
+| match_type  | Match on the "EPI_ISL_XXX" field, a regular expression, the strain, or the default | default, epi, regex, strain |  | default |
+| method | Method | local, mincut | :heavy_check_mark: | local |
+| no_collapse | No collapse | boolean | | |
+| output_filename | Output filename | string | :heavy_check_mark: | |
+| output_path | Output path | string | :heavy_check_mark: | |
+| p_value | P-value | float | | 0.001 |
+| ref_segment | Reference Segment | string | :heavy_check_mark: | HA |
+| ref_tree_inference | Reference tree inference method | FastTree, IQTree | | IQTree |
+| segments | Segments | string | :heavy_check_mark: | All segments |
+
+
+
+
+
+
---
+++ service-scripts/App-TreeSort.pl
@@ -0,0 +1,137 @@
+#
+# The TreeSort application
+#
+
+use Bio::KBase::AppService::AppScript;
+use Bio::KBase::AppService::AppConfig;
+
+use strict;
+use Data::Dumper;
+use File::Basename;
+use File::Slurp;
+use File::Temp;
+use LWP::UserAgent;
+use JSON::XS;
+use IPC::Run qw(run);
+use Cwd;
+use Clone;
+
+my $script = Bio::KBase::AppService::AppScript->new(\&process_treesort, \&preflight);
+
+my $rc = $script->run(\@ARGV);
+
+exit $rc;
+
+
+sub preflight
+{
+   # Declare and assign local variables for the parameters passed to the preflight function.
+   my($app, $app_def, $raw_params, $params) = @_;
+
+   print STDERR "Pre-flight TreeSort ", Dumper($params, $app);
+
+   return {
+      cpu => 2,
+      memory => "64G",
+      runtime => 18000,
+      storage => 0,
+   };
+}
+
+sub process_treesort
+{
+   # Declare and assign local variables for the parameters passed to the process_treesort function.
+   my($app, $app_def, $raw_params, $params) = @_;
+
+   # Uncomment to troubleshoot the parameters.
+   # warn Dumper($app_def, $raw_params, $params);
+
+   my $token = $app->token();
+   my $ws = $app->workspace();
+
+   # Create a temp directory for intermediate and result files.
+   my $cwd = File::Temp->newdir(CLEANUP => 1);
+
+   # Create an "input" subdirectory for the input FASTA file, etc.
+   my $input_dir = "$cwd/input";
+   -d $input_dir or mkdir $input_dir or die "Cannot mkdir $input_dir: $!";
+
+   # Create a "working" subdirectory for the input and intermediate file(s).
+   my $work_dir = "$cwd/work";
+   -d $work_dir or mkdir $work_dir or die "Cannot mkdir $work_dir: $!";
+   
+   # TODO: Are these needed?
+   my $data_api = Bio::KBase::AppService::AppConfig->data_api_url;
+   my $dat = { data_api => $data_api };
+   my $sstring = encode_json($dat);
+
+   # Clone the input parameters.
+   my $params_to_app = Clone::clone($params);
+
+   # Encode the input parameters as a job description JSON file.
+   my $job_desc = "$cwd/jobdesc.json";
+   open(JOB_DESC, ">", $job_desc) or die "Cannot write $job_desc: $!";
+   print JOB_DESC JSON::XS->new->pretty(1)->encode($params_to_app);
+   close(JOB_DESC);
+
+   my $parallel = $ENV{P3_ALLOCATED_CPU};
+
+   # Run the Python script that runs TreeSort.
+   my @cmd = ("run_treesort.py", "-i", $input_dir, "-j", $job_desc, "-w", $work_dir);
+   my $ok = run(\@cmd);
+
+   # Was the command successful?
+   if (!$ok)
+   {
+      die "Command failed: @cmd\n";
+      exit 1;
+   }
+
+   # A modifiable version of the workspace's result folder name.
+   my $result_folder = $app->result_folder;
+
+   # Make sure the result folder name doesn't end with "/.".
+   if ($result_folder =~ m{\/\.$})
+   {
+      $result_folder = substr $result_folder, 0, -2;
+   }
+
+   # Make sure the result folder starts with "ws:".
+   $result_folder = "ws:$result_folder" unless $result_folder =~ /^ws:/;
+
+   # Map file extensions to BV-BRC file types.
+   my %suffix_map = (aln => 'aligned_dna_fasta',
+                     csv => 'csv',
+                     pdf => 'pdf',
+                     tre => 'nwk',
+                     tsv => 'tsv');
+
+   my @suffix_map = map { ("--map-suffix", "$_=$suffix_map{$_}") } keys %suffix_map;
+
+   # If the result directory doesn't exist, create it.
+   if (! -d $result_folder) {
+
+      # Make sure the result folder (output path) exists.
+      my @cmd = ("p3-mkdir", $result_folder);
+      print "@cmd\n";
+
+      my $ok = IPC::Run::run(\@cmd);
+      if (!$ok)
+      {
+         die "Error $? creating directory $result_folder\n";
+         exit 1;
+      }
+   }
+
+   # Use the p3 utility to copy the files in the work directory to the user's workspace.
+   my @cmd = ("p3-cp", "-r", "-f", @suffix_map, "$work_dir", "$result_folder");
+   print "@cmd\n";
+   my $ok = IPC::Run::run(\@cmd);
+   if (!$ok)
+   {
+      warn "Error $? copying output with @cmd\n";
+      exit 1;
+   }
+   
+   exit 0;
+}
